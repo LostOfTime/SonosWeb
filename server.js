f894dc114bcd098b15c7ff4c9261c2e058507495
= <miceli.antoine@gmail.com>
2016-05-20 15:25:50 +0200

    Add swig and index html file

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
-var app = express();
+var app     = express();
+var swig    = require('swig');
+
+// configure template motor
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/static/views');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
